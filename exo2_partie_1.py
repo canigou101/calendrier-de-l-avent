e3d2460992705defffea00339f0a4a2988fce9b3
gabinGaucherBoissier <zombismini.101@gmail.com>
2024-12-06 01:20:56 +0100

    exo2 partie 1

--- exo2_partie_1.py
+++ exo2_partie_1.py
@@ -3,19 +3,23 @@ with open("input_exo2.txt","r") as f:
     liste_ligne=[]
     for line in lines: # Pour chaque ligne de la liste "lines"
         a = line.split()
-        liste_ligne.append(int(a))
+        liste_ligne.append(a)
     
-    def nb_occurences(chiffre,liste):
-        cp=0
-        for i in range(len(liste)):
-            if chiffre==liste[i]:
-                cp+=1
-        return cp
-    print(liste_ligne)
-    compteur_de_bon_rapport=0
+    nb_rapport_bon=0
+
+    def est_croissante(liste_d_element):
+        for i in range(len(liste_d_element)-1):
+            if int(liste_d_element[i])>int(liste_d_element[i+1]):
+                return 0
+        return 1
     
-    def est_croissante():
-    for element in liste_ligne:
-        if nb_occurences(element,liste_ligne)==1:
-            pass
-#faire la diff et cut si c'est pas bon 
+    def différence_entre_deux_sup(liste_d_element):
+        for i in range(len(liste_d_element)-1):
+            if abs(int(liste_d_element[i])-int(liste_d_element[i+1]))>3 or abs(int(liste_d_element[i])-int(liste_d_element[i+1]))<1 :
+                return 0
+        return 1
+
+    for i in range(len(liste_ligne)):
+        if (est_croissante(liste_ligne[i])==1 and différence_entre_deux_sup(liste_ligne[i])==1) or (est_croissante(liste_ligne[i][::-1])==1 and différence_entre_deux_sup(liste_ligne[i])==1):
+            nb_rapport_bon+=1
+    print(nb_rapport_bon)
