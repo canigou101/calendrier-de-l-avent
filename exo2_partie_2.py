6cd830154d483b53705f08c385976c86e70d60bc
gabinGaucherBoissier <zombismini.101@gmail.com>
2024-12-19 17:22:15 +0100

    exo2 partie 2 pas fini

--- exo2_partie_2.py
+++ exo2_partie_2.py
@@ -5,6 +5,20 @@ with open("input_exo2.txt","r") as f:
         a = line.split()
         liste_ligne.append(a)
     
+    nb_rapport_bon=0
+
+    def est_croissante(liste_d_element):
+        for i in range(len(liste_d_element)-1):
+            if int(liste_d_element[i])>int(liste_d_element[i+1]):
+                return 0
+        return 1
+    
+    def différence_entre_deux_sup(liste_d_element):
+        for i in range(len(liste_d_element)-1):
+            if abs(int(liste_d_element[i])-int(liste_d_element[i+1]))>3 or abs(int(liste_d_element[i])-int(liste_d_element[i+1]))<1 :
+                return 0
+        return 1
+    
     def nb_occurences(chiffre,liste):
         cp=0
         for i in range(len(liste)):
@@ -12,7 +26,15 @@ with open("input_exo2.txt","r") as f:
                 cp+=1
         return cp
     
-    """
-    si nb_occurence >2 next liste
-    sinon si nb_occurence <= 2 
-    """
+    def listeOccurence(ligne):
+        listeOcc=[]
+        for i in range(len(ligne)):
+            listeOcc.append(nb_occurences(ligne[i],ligne))
+        return listeOcc
+    
+    for i in range(len(liste_ligne)):
+        if (est_croissante(liste_ligne[i])==1 or est_croissante(liste_ligne[i][::-1])==1) and différence_entre_deux_sup(liste_ligne[i])==1:
+            nb_rapport_bon+=1
+        elif (est_croissante(liste_ligne[i])==0 or est_croissante(liste_ligne[i][::-1])==0) and différence_entre_deux_sup(liste_ligne[i])==0 and max(listeOccurence(liste_ligne[i]))<=2:
+            nb_rapport_bon+=1
+    print (nb_rapport_bon)
